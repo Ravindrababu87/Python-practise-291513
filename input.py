966b177b92201293c8fa2dc95801dbb7bb008d58
Ravindrababu87 <ravindradanduboyina2502@gmail.com>
2021-04-27 21:17:47 +0530

    taking the user input

---
+++ input.py
@@ -0,0 +1,10 @@
+n=input("Enter string:")
+if n== "lol":
+    print("laughing  out loud")
+
+if n== "rofl":
+    print("rolling on the floor laughing")
+if n== "lmk":
+    print("let me know")
+if n== "smh":
+    print("shaking my head")
